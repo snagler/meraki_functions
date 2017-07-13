32752adf0b0abd35ece252d126901dfbfcfa3f2e
James Stabler <james.stabler@westerville.org>
2017-07-13 12:37:01 -0400

    Added function
    Added Get-MerakiSwitchPorts

--- meraki_api_functions.ps1
+++ meraki_api_functions.ps1
@@ -134,3 +134,47 @@ function Get-MerakiOrganizations {
     return $request
 
 }
+
+function Get-MerakiSwitchPorts {
+
+    #Useage: Get-MerakiSwitchPorts "SW01"
+
+    param (
+    
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $switch_name
+    
+    )
+    
+    $switch = Get-MerakiSwitches | where {$_.name -eq $switch_name}
+
+    if ($switch){
+
+        $api = @{
+
+            "endpoint" = 'https://dashboard.meraki.com/api/v0'
+    
+        }
+
+        $header = @{
+        
+            "X-Cisco-Meraki-API-Key" = $api_key
+            "Content-Type" = 'application/json'
+        
+        }
+
+        $api.url = "/devices/" + $switch.serial + "/switchPorts"
+        $uri = $api.endpoint + $api.url
+        $request = Invoke-RestMethod -Method GET -Uri $uri -Headers $header
+        return $request
+    
+    }
+    else{
+
+        Write-Host "Switch doesn't exist." -ForegroundColor Red
+    
+    }
+
+}
